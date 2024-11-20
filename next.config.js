19e1e7de88a2c45a1c48bebaacfd19278588cc67
Pablo <pasha20030407@gmail.com>
2024-11-20 17:18:00 +0200

    test5

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
 
 module.exports = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()];
-    }
+  webpack: (config) => {
 
     return config;
   },
